ac1e1c387c8416caf45a21add087665011983fde
Cpt_Kirk <121898802+cptkirki@users.noreply.github.com>
2025-08-10 13:32:01 +0200

    tof_vl53l1x.h

--- tof_vl53l1x/tof_vl53l1x.h
+++ tof_vl53l1x/tof_vl53l1x.h
@@ -1,4 +1,4 @@
-// /config/my_components/tof_vl53l1x/tof_vl53l1x.h
+// in esphome directory /my_components/tof_vl53l1x/tof_vl53l1x.h
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
